99821d8b4267123e13ff8e40fc2608f78ae2ef84
Brian Duggan <brian@promptworks.com>
2016-07-18 14:32:31 -0400

    busted tests

---
+++ test/test_spec.lua
@@ -0,0 +1,8 @@
+describe("some assertions", function()
+  it("tests positive assertions", function()
+    assert.is_true(true)  -- Lua keyword chained with _
+    assert.True(true)     -- Lua keyword using a capital
+    assert.are.equal(1, 1)
+    assert.has.errors(function() error("this should fail") end)
+  end)
+end)
