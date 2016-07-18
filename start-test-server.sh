90bbb1ac916067506a547c4f28e925acf54cb1f8
Brian Duggan <brian@promptworks.com>
2016-07-18 15:53:38 -0400

    temp path

--- start-test-server.sh
+++ start-test-server.sh
@@ -4,7 +4,10 @@ PATH=/usr/local/openresty/nginx/sbin:$PATH
 PORT=${PORT:-1234}
 conf=nginx.conf.compiled
 PORT=$PORT perl -lpe 's/\$\{\{PORT\}\}/$ENV{PORT}/' nginx.conf > $conf
+
 mkdir -p logs
+mkdir -p temp
+
 touch logs/access.log
 touch logs/error.log
 nginx -p "`pwd`" -c $conf &
--- t/00-connect.t
+++ t/00-connect.t
@@ -5,9 +5,11 @@ use Test::More;
 
 ok 1, "Testing";
 
+diag "port is $ENV{PORT}";
+
 my $t = Test::Mojo->new;
 
-$t->get_ok("http://localhost:$ENV{PORT}")
+$t->get_ok("http://127.0.0.1:$ENV{PORT}")
   ->status_is(200)
   ->content_is("hello, world\n");
 
